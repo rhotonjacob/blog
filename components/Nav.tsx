3a6d0e0e89ffdd2f1d45515b3ea6cdca94a20418
Jacob Rhoton <rhoton.jacob@gmail.com>
2023-02-26 14:56:31 -0700

    Set 100vh on sections; address build failures

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,14 +2,16 @@ import {Flex, HStack, Link, Square} from "@chakra-ui/react"
 
 const Nav = () => {
   return (
-    <Flex justifyContent="space-between" p="1rem" bg="gray.100">
-      <Square size="40px" bg="black" />
+    <Flex position="fixed" top="0" width="100%" justifyContent="space-between" p="1rem" bg="gray.100">
+      <Link href="/">
+        <Square size="40px" bg="black" />
+      </Link>
       <HStack spacing="1rem">
         <Link href="#about">About</Link>
         <Link href="#work">Work</Link>
         <Link href="#portfolio">Portfolio</Link>
         <Link href="#contact">Contact</Link>
-        <Link href="/resume.pdf" target="_blank">Resume</Link>
+        <Link href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</Link>
       </HStack>
     </Flex>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Link } from '@chakra-ui/react'
+import Head from "next/head"
+import { Box, Container, Flex } from '@chakra-ui/react'
 import Nav from '../components/Nav';
 
 interface Position {
@@ -39,32 +40,46 @@ const workHistory: Position[] = [
 const Home = () => {
   return (
     <>
-      {/* <nav>
-        <Link href="#home">Home</Link>
-        <Link href="#about">About</Link>
-        <Link href="#experience">Experience</Link>
-      </nav> */}
+      <Head>
+        <title>Jacob Rhoton</title>
+      </Head>
       <Nav />
       <main>
-        <section id="home">
-          <h1>Welcome to my resume site!</h1>
+        <section>
+          <Flex alignItems="center" justifyContent="center" height="100vh">
+            <h1>Welcome to my resume site!</h1>
+          </Flex>
         </section>
         <section id="about">
-          <h2>About me</h2>
-          <p>I am a Software Engineer from Denver, Colorado with over three years of professional web development experience, primarily using React and TypeScript. The most rewarding part of this career, for me, is having regular opportunities to learn and grow in order to overcome new challenges.</p>
+          <Flex alignItems="center" justifyContent="center" height="100vh">
+            <h2>About me</h2>
+            <p>I am a Software Engineer from Denver, Colorado with over three years of professional web development experience, primarily using React and TypeScript. The most rewarding part of this career, for me, is having regular opportunities to learn and grow in order to overcome new challenges.</p>
+          </Flex>
         </section>
-        <section id="experience">
-          <h2>Experience</h2>
-          {workHistory.map(({title, company, url, duration, bulletPoints}, jobIndex) => (
-            <div key={jobIndex}>
-              <h2 style={{display: "inline-block"}}>{`${title} @ `}</h2>
-              <a href={url} target="_blank" style={{display: "inline-block"}}>{company}</a>
-              <p>{duration}</p>
-              {bulletPoints.map((item, i) => (
-                <p key={i}>{`- ${item}`}</p>
-              ))}
-            </div>
-          ))}
+        <section id="work">
+          <Flex alignItems="center" justifyContent="center" height="100vh">
+            <h2>Experience</h2>
+            {workHistory.map(({title, company, url, duration, bulletPoints}, jobIndex) => (
+              <div key={jobIndex}>
+                <h2 style={{display: "inline-block"}}>{`${title} @ `}</h2>
+                <a href={url} target="_blank" style={{display: "inline-block"}}>{company}</a>
+                <p>{duration}</p>
+                {bulletPoints.map((item, i) => (
+                  <p key={i}>{`- ${item}`}</p>
+                ))}
+              </div>
+            ))}
+          </Flex>
+        </section>
+        <section id="portfolio">
+          <Flex alignItems="center" justifyContent="center" height="100vh">
+            <h1>Portfolio</h1>
+          </Flex>
+        </section>
+        <section id="contact">
+          <Flex alignItems="center" justifyContent="center" height="100vh">
+            <h1>Contact</h1>
+          </Flex>
         </section>
       </main>
 
