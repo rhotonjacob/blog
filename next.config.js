8daf6a549478829a2f7c762bfe92380064fe50b2
Jacob Rhoton <rhoton.jacob@gmail.com>
2023-08-21 10:32:28 -0600

    Configure Sass

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true
-  }
-}
+    appDir: true,
+  },
+  sassOptions: {
+    logger: {
+      warn: (message) => console.warn(message),
+      debug: (message) => console.log(message),
+    },
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
